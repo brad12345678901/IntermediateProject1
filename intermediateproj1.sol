f89fe693ed2b61ca3a2624f728f0f8bae4f346e6
brad675 <90821323+brad12345678901@users.noreply.github.com>
2024-07-10 23:20:23 +0800

    Add files via upload

--- intermediateproj1.sol
+++ intermediateproj1.sol
@@ -1,35 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract TokenV2 {
+contract ATM {
 
-    // public variables here
-    string public TokenName = "Truely Amazing Coin";
-    string public TokenAbbrev = "TAC";
-    uint public TotalSupply;
+    uint constant private limit = 50000;
+    address private user;
     // mapping variable here
-    mapping(address => uint) public balances;
-    // mint function
-    function mint(address to, uint value) public {
-        require(value > 0, "Mint amount must be greater than zero");
-        TotalSupply += value;
-        balances[to] += value;
-        if (value > 50){
-            revert("Minting was limited to 50");
+    mapping(address => uint) private balances;
+    constructor (address u){
+        user = u;
+    }
+    // deposit function
+    function deposit(uint value) public {
+        require(value > 0, "The amount must be greater than zero");
+        require(value <= limit, "Only up to 50K that can be deposited");
+        balances[user] += value;
+        if (balances[user] > limit){
+            revert("Insuficient Spaces for Deposit, Consider Withdrawing to provide space");
         }
     }
-    // burn function
-    function burn(address from, uint value) public {
-        require(balances[from] >= value, "Insufficient balance for burning");
-        TotalSupply -= value;
-        balances[from] -= value;
-        assert(TotalSupply == balances[from]);
+    function viewBalance() public view returns (uint){
+        return balances[user];
     }
-    //test function to test assertion
-    function testassert(uint value) public {
-        TotalSupply -= value;
-    }
-    function matchTotalSupply(address from) public {
-        TotalSupply = balances[from];
+    // withdraw function
+    function withdraw(uint value) public {
+        assert(balances[user] != 0);
+        require(value > 0, "The amount must be greater than zero");
+        require(value <= limit, "Only up to 50K that can be withdrawn");
+        require(value <= balances[user],"The amount to withdraw exceeds your balance");
+        balances[user] -= value;
     }
 }
